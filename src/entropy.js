086ab0e414cdd3646ab383219a8ffd497ba47bbf
Sergey Romanov <xxsmotur@gmail.com>
2015-06-27 22:23:10 +0500

    add partial conditional entropy

--- src/entropy.js
+++ src/entropy.js
@@ -1,20 +1,56 @@
+
+import underscore, {each, range} from 'underscore'
 export default {
     
     calc: function(items){
-        items = items.split('');
-        let count = items.length;
-        let data = counter(items);
+        let data = prepare(items);
         let result = 0;
+        let count = items.length;
         for (let [ key, val ] of data.entries()){
             let probability = val/count;
             result += probability * Math.log2(probability);
         }
         return -result;
     },
+
+    conditional: function(X, Y){
+        let data1 = prepare(X);
+        let data2 = prepare(Y);
+        let result = 0;
+        for(let [key, val] of joint_probability(X,Y)){
+            let probX = data1.get(key[0]);
+            result += val * Math.log(probX/val);
+        };
+        return result;
+    }
 }
 
+var joint_probability = function(X, Y){
+    let len = X.length;
+    let result = 0;
+    X = X.split('');
+    Y = Y.split('');
+    var m = new Map();
+    each(range(len), i => {
+        let x = X[i];
+        let y = Y[i];
+        if(m.get((x,y)) == undefined){
+            let res = X.filter(v => v == x && Y[i] == y).length/len;
+            m.set([x, y], res);
+        }
+
+    });
+    return m
+
+}
 var prob = function(items, num){
     return items.filter(x => x == num).length/items.length;
+}
+
+var prepare = function(items){
+    items = items.split('');
+    return counter(items);
+
 }
 
 var counter = function(items){
