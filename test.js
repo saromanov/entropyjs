d7e9cf4d43e91052082aeecb61d72a8a159270eb
Sergey Romanov <xxsmotur@gmail.com>
2015-06-29 23:24:11 +0500

    add test

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ describe('Test basic Shannon entropy', function(){
     it('should compute entropy', function(){
        assert(entropy.calc("abcdeabcdefj").length != 0); 
     });
+
+    it('should compute conditional shannon entropy', function(){
+        assert(entropy.jointShannon('010101','100101').length != 0);
+    });
 });
