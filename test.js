54444a6e1783358ffe166cb0579f58d3c3af695c
Sergey Romanov <xxsmotur@gmail.com>
2015-07-02 22:55:00 +0500

    add test for cross entropy

--- test.js
+++ test.js
@@ -3,10 +3,17 @@ var entropy = require('./index.js');
 
 describe('Test basic Shannon entropy', function(){
     it('should compute entropy', function(){
-       assert(entropy.calc("abcdeabcdefj").length != 0); 
+       assert(entropy.shannon("abcdeabcdefj").length != 0); 
     });
 
     it('should compute conditional shannon entropy', function(){
         assert(entropy.jointShannon('010101','100101').length != 0);
+    });
+});
+
+
+describe('Test cross entropy', function(){
+    it('should compute conditional entropy', function(){
+        assert(entropy.cross([1,1,2],[1,1,3]).length != 0);
     });
 });
